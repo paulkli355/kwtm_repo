9b040bba493e6a75e000cd87cf6b8738efb50cd7
student <student@mini-12.local>
2022-03-17 02:48:52 -0700

    second commit after generating project

---
+++ projekt/build/CMakeFiles/Laborka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laborka.dir/main.cpp.o"
+  "Laborka.pdb"
+  "Laborka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laborka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projekt/build/CMakeFiles/Laborka.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Laborka.
+# This may be replaced when dependencies are built.
