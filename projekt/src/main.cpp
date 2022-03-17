c64e1b9ef5eb5d47163091938cf473a9a528f8fd
student <student@mini-12.local>
2022-03-17 04:16:19 -0700

    projekt z wlaczona opcja

--- projekt/src/main.cpp
+++ projekt/src/main.cpp
@@ -3,20 +3,18 @@
 #ifdef USE_TRIGONOMETRY_DEGREE
     #include "trygonometria.h"
 #endif
+#include <cmath>
 
 int main(){
 
-    double x = degreemath::sin(90.0);
-    std::cout << x << std::endl;
-
     double result = 0;
     #ifdef USE_TRIGONOMETRY_DEGREE
-        result = cos_degree(45.0); 
+        result = degreemath::cos(45.0); 
     #else
-        result = cos(pi/4.0); 
+        result = cos(M_PI/4.0); 
     #endif
 
-    // std::cout << result << std::endl;
+    std::cout << result << std::endl;
     return result;
 
 }
